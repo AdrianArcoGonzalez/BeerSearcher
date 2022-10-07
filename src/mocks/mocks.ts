3a74f8a1d4f9ce61bf14d0e43dd3b99585b67ce3
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-10-07 13:55:16 +0200

    add mocked beers array

--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -9,4 +9,4 @@ export const mockBeer1: Beer = {
     "https://www.wbbrew.com/wp-content/uploads/2017/07/WBB_GOB_500_bottle-400x400.jpg",
 };
 
-const mockBeerArray = [mockBeer1, mockBeer1, mockBeer1];
+export const mockBeerArray = [mockBeer1, mockBeer1, mockBeer1];
