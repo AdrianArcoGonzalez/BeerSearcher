c99f6818856a18cd99dd30b9bc3bdc1aabdd6683
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-10-07 13:54:48 +0200

    create mocked beers

---
+++ src/mocks/mocks.ts
@@ -0,0 +1,12 @@
+import { Beer } from "../interfaces";
+
+export const mockBeer1: Beer = {
+  id: 1,
+  description:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem.",
+  name: "Beer 1",
+  imageSrc:
+    "https://www.wbbrew.com/wp-content/uploads/2017/07/WBB_GOB_500_bottle-400x400.jpg",
+};
+
+const mockBeerArray = [mockBeer1, mockBeer1, mockBeer1];
