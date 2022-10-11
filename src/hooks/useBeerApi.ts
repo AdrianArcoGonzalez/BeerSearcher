6310bd8943aa14d1b8b4d9ce54de56dd8e789779
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-10-11 18:01:41 +0200

    add random beer function on custom hook

--- src/hooks/useBeerApi.ts
+++ src/hooks/useBeerApi.ts
@@ -1,13 +1,19 @@
 import axios from "axios";
+import { useCallback } from "react";
+import { errorModal, loadingModal } from "../components/Modals/Modals";
 import { Beer } from "../interfaces/interfaces";
-import { useAppDispatch } from "../store/hooks";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
 import { randomBeerActionCreator } from "../store/slices/beersSlice";
 import environments from "../utils/environments";
 
 const useBeerApi = () => {
   const dispatch = useAppDispatch();
+  const { randomBeer } = useAppSelector((state) => state.beers);
+  let randomNumber = 0;
 
-  const getRandomBeer = async () => {
+  const getRandomBeer = useCallback(async () => {
+    loadingModal("Loading...");
+
     try {
       const response = await axios(environments.randomBeer);
       const data: Beer = response.data[0];
@@ -25,9 +31,33 @@ const useBeerApi = () => {
 
       dispatch(randomBeerActionCreator(newBeer));
     } catch (error) {}
-  };
+  }, [dispatch]);
 
-  return { getRandomBeer };
+  const getNonAlcoholicBeer = useCallback(async () => {
+    loadingModal("Loading...");
+
+    try {
+      let response = await axios(environments.noAlcoholBeer);
+      let data: Beer = response.data[randomNumber];
+
+      if (data.id === randomBeer.id) {
+        response = await axios(environments.noAlcoholBeer);
+        data = response.data[randomNumber + 1];
+      }
+      const newBeer: Beer = {
+        description: data.description,
+        image_url: data.image_url,
+        name: data.name,
+        id: data.id,
+      };
+
+      dispatch(randomBeerActionCreator(newBeer));
+    } catch (error) {
+      errorModal("Cannot get a new beer now");
+    }
+  }, [dispatch, randomBeer.id, randomNumber]);
+
+  return { getRandomBeer, getNonAlcoholicBeer };
 };
 
 export default useBeerApi;
